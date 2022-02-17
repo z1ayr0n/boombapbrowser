7678b0cf9f2a1486c06eb1ddaaae34d2e9318ec4
ayr0n <2infamouz@gmail.com>
2022-02-17 00:14:02 -0500

    Pad Settings / Back Panel
    Added settings panel and individual sample rate parameters that user can control for each pad.

--- script.js
+++ script.js
@@ -26,6 +26,25 @@ const powerButton = document.getElementById('powerButton');
 var hasPower = false;
 var isInit = false;
 
+//Settings Button
+const settingsButton = document.getElementById('settingsButton');
+
+//Panel references
+const frontPanel = document.getElementById('frontPanel');
+const backPanel = document.getElementById('backPanel');
+
+settingsButton.addEventListener('click', function() {
+    frontPanel.setAttribute("style", "visibility: hidden");
+    backPanel.setAttribute("style", "visibility: visible");
+}, false);
+
+//Return Button
+const returnButton = document.getElementById('returnButton');
+returnButton.addEventListener('click', function() {
+    frontPanel.setAttribute("style", "visibility: visible");
+    backPanel.setAttribute("style", "visibility: hidden");
+}, false);
+
 powerButton.addEventListener('click', function() {
     if(!isInit){
         initializeProgram();
@@ -47,6 +66,11 @@ const padButtonA = document.getElementById('padButtonA');
 const padButtonB = document.getElementById('padButtonB');
 const padButtonC = document.getElementById('padButtonC');
 const padButtonD = document.getElementById('padButtonD');
+//References to rate controllers
+const aRateControl = document.getElementById('aRate');
+const bRateControl = document.getElementById('bRate');
+const cRateControl = document.getElementById('cRate');
+const dRateControl = document.getElementById('dRate');
 
 function initializeProgram(){
     //cross browser audio context
@@ -57,6 +81,58 @@ function initializeProgram(){
     bandpass.type = 'bandpass';
     //Create master gain node
     var mGainNode = audioContext.createGain();
+    //Individual pad gains
+    var aGainNode = audioContext.createGain();
+    var bGainNode = audioContext.createGain();
+    var cGainNode = audioContext.createGain();
+    var dGainNode = audioContext.createGain();
+    function padGainSelect(pad){
+        switch (pad) {
+            case 0:
+                return aGainNode;
+            case 1:
+                return bGainNode;
+            case 2:
+                return cGainNode;
+            case 3:
+                return dGainNode;
+            default:
+                console.log("invalid pad number entered");
+                break;
+        }
+    }
+    //Sample rate for pads
+    let aSampleRate = 1;
+    let bSampleRate = 1;
+    let cSampleRate = 1;
+    let dSampleRate = 1;
+    function padRateSelect(pad){
+        switch (pad) {
+            case 0:
+                return aSampleRate;
+            case 1:
+                return bSampleRate;
+            case 2:
+                return cSampleRate;
+            case 3:
+                return dSampleRate;
+            default:
+                console.log("invalid pad number entered");
+                break;
+        }
+    }
+    aRateControl.addEventListener('input', function() {
+        aSampleRate = Number(this.value);
+    }, false);
+    bRateControl.addEventListener('input', function() {
+        bSampleRate = Number(this.value);
+    }, false);
+    cRateControl.addEventListener('input', function() {
+        cSampleRate = Number(this.value);
+    }, false);
+    dRateControl.addEventListener('input', function() {
+        dSampleRate = Number(this.value);
+    }, false);
 
     //Load samples to buffer and decode
     async function getFile(audioContext, filepath) {
@@ -84,31 +160,32 @@ function initializeProgram(){
     setupSample()
     .then((sampleArray) => {
 
-        function playSample(audioContext, audioBuffer) {
+        function playSample(audioContext, audioBuffer, pad) {
             const sampleSource = audioContext.createBufferSource();
             sampleSource.buffer = audioBuffer;
+            sampleSource.playbackRate.value = padRateSelect(pad);
             bandpass.frequency.value = mFilterFrequency;
             mGainNode.gain.value = masterGainValue;
-            sampleSource.connect(bandpass).connect(mGainNode).connect(audioContext.destination)
+            sampleSource.connect(padGainSelect(pad)).connect(bandpass).connect(mGainNode).connect(audioContext.destination)
             sampleSource.start();
             return sampleSource;
         }
         //Mousedown events for pads
         padButtonA.addEventListener('mousedown', function() {
             padButtonA.setAttribute("style", "background-image: url('image/ui/front/drumpad-press.png')");
-            playSample(audioContext, sampleArray[0]);
+            playSample(audioContext, sampleArray[0], 0);
         }, false);
         padButtonB.addEventListener('mousedown', function() {
             padButtonB.setAttribute("style", "background-image: url('image/ui/front/drumpad-press.png')");
-            playSample(audioContext, sampleArray[1]);
+            playSample(audioContext, sampleArray[1], 1);
         }, false);
         padButtonC.addEventListener('mousedown', function() {
             padButtonC.setAttribute("style", "background-image: url('image/ui/front/drumpad-press.png')");
-            playSample(audioContext, sampleArray[2]);
+            playSample(audioContext, sampleArray[2], 2);
         }, false);
         padButtonD.addEventListener('mousedown', function() {
             padButtonD.setAttribute("style", "background-image: url('image/ui/front/drumpad-press.png')");
-            playSample(audioContext, sampleArray[3]);
+            playSample(audioContext, sampleArray[3], 3);
         }, false);
 
         //Mouseup events for pads
@@ -135,25 +212,25 @@ function initializeProgram(){
         case "s":
             // code for "down arrow" key press.
             padButtonA.setAttribute("style", "background-image: url('image/ui/front/drumpad-press.png')");
-            playSample(audioContext, sampleArray[0]);
+            playSample(audioContext, sampleArray[0], 0);
             break;
         case "ArrowUp":
         case "w":
             // code for "up arrow" key press.
             padButtonB.setAttribute("style", "background-image: url('image/ui/front/drumpad-press.png')");
-            playSample(audioContext, sampleArray[1]);
+            playSample(audioContext, sampleArray[1], 1);
             break;
         case "ArrowLeft":
         case "a":
             // code for "left arrow" key press.
             padButtonC.setAttribute("style", "background-image: url('image/ui/front/drumpad-press.png')");
-            playSample(audioContext, sampleArray[2]);
+            playSample(audioContext, sampleArray[2], 2);
             break;
         case "ArrowRight":
         case "d":
             // code for "right arrow" key press.
             padButtonD.setAttribute("style", "background-image: url('image/ui/front/drumpad-press.png')");
-            playSample(audioContext, sampleArray[3]);
+            playSample(audioContext, sampleArray[3], 3);
             break;
         default:
             return; // Quit when this doesn't handle the key event.
